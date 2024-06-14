8326c1b9e277c7f3a63dda67741727889eec1c7f
ENDISit <mr.ilia08@mail.ru>
2024-06-14 22:46:32 +0300

    Добавьте файлы проекта.

---
+++ Task 2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] strings = { "apple", "banana", "cherry", "date", "fig", "grape" };
+
+        Console.Write("Введите строку для поиска: ");
+        string inputString = Console.ReadLine();
+
+        bool found = false;
+        foreach (string str in strings)
+        {
+            if (str == inputString)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine("Строка найдена!");
+        }
+        else
+        {
+            Console.WriteLine("Строка не найдена!");
+        }
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
